b93e1f776452461f85bd20f6629b6b21c283528b
Sorin Chis <kish.sorin@yahoo.com>
2019-03-19 16:11:44 +0200

    added put endpoint request with async method

--- routes.js
+++ routes.js
@@ -134,4 +134,47 @@ routes.delete('/', async (req, res) => {
 	}
 });
 
+// routes.put('/:id', (req, res) => {
+// 	const { id } = req.params;
+// 	const post = req.body;
+
+// 	if (post.title && post.contents) {
+// 		db
+// 			.update(id, post)
+// 			.then((data) => {
+// 				if (data) {
+// 					res.status(200).json(data);
+// 				} else {
+// 					res.status(404).json({ message: 'The post with the specified ID does not exist.' });
+// 				}
+// 			})
+// 			.catch((err) => {
+// 				res.status(500).json({ error: 'The post information could not be modified.' });
+// 			});
+// 	} else {
+// 		res.status(400).json({ errorMessage: 'Please provide title and contents for the post.' });
+// 	}
+// });
+
+routes.put('/:id', async (req, res) => {
+	const { id } = req.params;
+	const post = req.body;
+
+	try {
+		if (post.title && post.contents) {
+			const result = await db.update(id, post);
+
+			if (result.length) {
+				res.status(200).json(result);
+			} else {
+				res.status(404).json({ message: 'The post with the specified ID does not exist.' });
+			}
+		} else {
+			res.status(400).json({ errorMessage: 'Please provide title and contents for the post.' });
+		}
+	} catch (error) {
+		res.status(500).json({ error: 'The post information could not be modified.' });
+	}
+});
+
 module.exports = routes;
